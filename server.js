72947a9f7d039b4ca9492240da455d1fced90a54
Zachary Gomez <zgomez301@gmail.com>
2021-09-14 18:38:36 -0700

    added ejs

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ const app = express()
 // Port def
 const port = process.env.PORT || 3002
 
+// Import Drinks Data
+const drinks = require("./models/drinks.js")
+
+
+// Routes
 app.get('/', (req, res) => {
     res.send(`Welcome to the Gitpub App!`)
+})
+
+app.get('/drinks', (req, res) => {
+    res.render('drinks_index.ejs')
 })
 
 app.listen(port, () => {
