6c523ac71977a334a3b2dd2397a76091f25f4a4f
Zachary Gomez <zgomez301@gmail.com>
2021-09-14 22:23:34 -0700

    added images

--- server.js
+++ server.js
@@ -22,8 +22,10 @@ app.get('/drinks', (req, res) => {
 })
 
 app.get('/drinks/:id', (req, res) => {
-    res.send(req.params.id)
-})
+    res.render('drinks_show.ejs', { //second param must be an object
+        drink: drinks[req.params.id] //there will be a variable available inside the ejs file called drinks, its value is drink[req.params.id]
+    })
+});
 
 app.listen(port, () => {
     console.log("Express is listening on port ", port)
