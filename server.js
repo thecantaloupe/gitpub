59e97182a682bc982a9908bca0cf2b0e4d2e3d4d
Zachary Gomez <zgomez301@gmail.com>
2021-09-14 22:35:37 -0700

    finished

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express()
 // Port def
 const port = process.env.PORT || 3002
 
-// Import Drinks Data
+// Import  Data
 const drinks = require("./models/drinks.js")
+const foods = require("./models/food.js")
 
 
 // Routes
@@ -24,6 +25,16 @@ app.get('/drinks', (req, res) => {
 app.get('/drinks/:id', (req, res) => {
     res.render('drinks_show.ejs', { //second param must be an object
         drink: drinks[req.params.id] //there will be a variable available inside the ejs file called drinks, its value is drink[req.params.id]
+    })
+});
+
+app.get('/foods', (req, res) => {
+    res.render('foods_index.ejs',{allFoods: foods})
+})
+
+app.get('/foods/:id', (req, res) => {
+    res.render('foods_show.ejs', { //second param must be an object
+        food: foods[req.params.id] //there will be a variable available inside the ejs file called foods, its value is food[req.params.id]
     })
 });
 
